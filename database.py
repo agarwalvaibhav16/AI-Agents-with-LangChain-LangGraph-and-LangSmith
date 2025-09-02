96579bf3453478139be4bbd2a44dfa82b7cf760e
vaibhav agarwal <vaibhavagarwal@vaibhavs-MacBook-Air-2.local>
2025-09-02 17:47:48 -0600

    adding routers

--- database.py
+++ database.py
@@ -7,3 +7,5 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
